ba2597ea98b0936773c7726eee2e97fa6092ba6d
Lukasz Blachnicki <lblachnicki@gmail.com>
2021-10-27 18:17:44 +0200

    Add settings colors

--- lab3/3_2/3_2.ino
+++ lab3/3_2/3_2.ino
@@ -5,16 +5,25 @@
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 #define RED_BUTTON_PIN 2
+#define GREEN_BUTTON_PIN 4
 
 #define RED 6
 #define GREEN 5
 #define BLUE 3
 
+int greenButtonState = HIGH;
+int lastGreenButtonState = HIGH;
+
 int redButtonState = HIGH;
 int lastRedButtonState = HIGH;
 
+unsigned long lastGreenDebounceTime = 0;
 unsigned long lastRedDebounceTime = 0;
 unsigned long debounceDelay = 50;
+
+unsigned long red_val = 0;
+unsigned long green_val = 0;
+unsigned long blue_val = 0;
 
 int color_index = 0;
 
@@ -22,6 +31,7 @@ void setup()
 {
   Serial.begin(9600);
 
+  pinMode(GREEN_BUTTON_PIN, INPUT_PULLUP);
   pinMode(RED_BUTTON_PIN, INPUT_PULLUP);
 
   pinMode(RED, OUTPUT); 
@@ -75,11 +85,42 @@ void output(long red, long green, long blue, int color_index) {
   lcd.setCursor(1,1);
   lcd.print(red);
 
+  if(red < 10) {
+    lcd.setCursor(2,1);
+    lcd.print(" ");
+  }
+
+  if(red < 100) {
+    lcd.setCursor(3,1);
+    lcd.print(" ");
+  }
+
   lcd.setCursor(5,1);
   lcd.print(green);
 
+  if(green < 10) {
+    lcd.setCursor(6,1);
+    lcd.print(" ");
+  }
+
+  if(green < 100) {
+    lcd.setCursor(7,1);
+    lcd.print(" ");
+  }
+
   lcd.setCursor(11,1);
   lcd.print(blue);
+
+  if(blue < 10) {
+    lcd.setCursor(12,1);
+    lcd.print(" ");
+  }
+
+  if(blue < 100) {
+    lcd.setCursor(13,1);
+    lcd.print(" ");
+  }
+
 
   switch(color_index){
     case 0: {
@@ -97,15 +138,35 @@ void output(long red, long green, long blue, int color_index) {
   }
 }
 
+unsigned long addition = 5;
+
 void loop()
 {
-  output(255, 255, 255, color_index);
+  output(red_val, green_val, blue_val, color_index);
 
+  analogWrite(RED, red_val);
+  analogWrite(GREEN, green_val);
+  analogWrite(BLUE, blue_val);
+
+  int greenReading = digitalRead(GREEN_BUTTON_PIN);
   int redReading = digitalRead(RED_BUTTON_PIN);
+
+if (greenReading != lastGreenButtonState)
+  {
+    lastGreenDebounceTime = millis();
+  }
 
   if (redReading != lastRedButtonState)
   {
     lastRedDebounceTime = millis();
+  }
+
+   if ((millis() - lastGreenDebounceTime) > debounceDelay)
+  {
+    if (greenReading != greenButtonState)
+    {
+      greenButtonState = greenReading;
+    }
   }
 
   if ((millis() - lastRedDebounceTime) > debounceDelay)
@@ -113,6 +174,25 @@ void loop()
     if (redReading != redButtonState)
     {
       redButtonState = redReading;
+    }
+  }
+
+  if(greenReading != lastGreenButtonState) {
+    if(greenReading == LOW) {
+      switch(color_index){
+    case 0: {
+      red_val = (red_val + addition) % 256;
+      break;
+    }
+    case 1: {
+      green_val = (green_val + addition) % 256;
+      break;
+    }
+    case 2: {
+      blue_val = (blue_val + addition) % 256;
+      break;
+    }
+  }
     }
   }
 
